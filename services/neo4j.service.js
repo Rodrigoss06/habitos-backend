bf9730708c11cadf1d1b64a32c3fc71c42fbf172
Rodrigoss06 <102317296+Rodrigoss06@users.noreply.github.com>
2025-06-02 00:01:13 -0500

    asdsadsad

--- services/neo4j.service.js
+++ services/neo4j.service.js
@@ -4,12 +4,12 @@ const uri = process.env.NEO4J_URI;
 const user = process.env.NEO4J_USER;
 const password = process.env.NEO4J_PASSWORD;
 
-if (!uri || !user || !password) {
-  throw new Error('Faltan variables de entorno para Neo4j');
-}
 console.log('NEO4J_URI:', process.env.NEO4J_URI);
 console.log('NEO4J_USERNAME:', process.env.NEO4J_USER);
 console.log('NEO4J_PASSWORD:', process.env.NEO4J_PASSWORD ? '****' : 'no definido');
+if (!uri || !user || !password) {
+  throw new Error('Faltan variables de entorno para Neo4j');
+}
 
 const driver = neo4j.driver(uri, neo4j.auth.basic(user, password));
 
