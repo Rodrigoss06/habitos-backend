f6cc292aefe1ca86be43d502fb9b1ef56c392010
Rodrigoss06 <102317296+Rodrigoss06@users.noreply.github.com>
2025-06-01 23:33:12 -0500

    V0.1.2: regresamos al start anterior y cambie el service para evitar error con las variables de entorno

--- services/neo4j.service.js
+++ services/neo4j.service.js
@@ -1,8 +1,9 @@
 import neo4j from 'neo4j-driver';
 
-const driver = neo4j.driver(
-  process.env.NEO4J_URI,
-  neo4j.auth.basic(process.env.NEO4J_USER, process.env.NEO4J_PASSWORD)
-);
+const uri = process.env.NEO4J_URI;
+const user = process.env.NEO4J_USERNAME;
+const password = process.env.NEO4J_PASSWORD;
+
+const driver = neo4j.driver(uri, neo4j.auth.basic(user, password));
 
 export const getSession = () => driver.session();
