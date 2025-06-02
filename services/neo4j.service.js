b1cbcdafe043cf39651279c532db2d739a308b5f
Rodrigoss06 <102317296+Rodrigoss06@users.noreply.github.com>
2025-06-01 23:50:50 -0500

    V0.1.5: correccion de variable env

--- services/neo4j.service.js
+++ services/neo4j.service.js
@@ -1,7 +1,7 @@
 import neo4j from 'neo4j-driver';
 
 const uri = process.env.NEO4J_URI;
-const user = process.env.NEO4J_USERNAME;
+const user = process.env.NEO4J_USER;
 const password = process.env.NEO4J_PASSWORD;
 
 if (!uri || !user || !password) {
